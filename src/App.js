e2a14d627561913e0235a830b4dd23434da6fc2f
CarlosHWolox <carlos.hernandez@wolox.cl>
2020-05-04 10:23:18 -0400

    Added stream-chat and chat-widget to base create react app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Widget } from 'react-chat-widget';
+
+import 'react-chat-widget/lib/styles.css';
+
+function App() {  
+  return (
+    <div className="App">
+      <Widget />
+    </div>
+  );
+}
+
+export default App;
